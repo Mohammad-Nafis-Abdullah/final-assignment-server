7f85075d0c5dded0c45d595a686818dfb3f6c94e
Mohammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2024-01-07 16:55:04 +0600

    server deploy with db functionalities

--- index.js
+++ index.js
@@ -5,11 +5,13 @@
 const successFn = require("./successFn");
 const DB = require("./mongodb");
 const express = require("express");
+const cors = require("cors");
 const PORT = 5000;
 
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const db = new DB("final-assignment");
 
